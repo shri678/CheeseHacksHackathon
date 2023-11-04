016be28ebbaefe6532ff1c79bced3a2eb0830eb3
Wil Cram <wcram17@gmail.com>
2023-11-04 14:55:27 -0500

    frontend init

---
+++ frontend/src/lib/components/Bet.svelte
@@ -0,0 +1,37 @@
+<script>
+    const options = ["Points", "Steals", "Rebounds"];
+    const overUnder = ["Over", "Under"];
+    let selectedOption = options[0];
+    let selectedOverUnder = overUnder[0];
+    let inputValue = '0';
+
+    function cleanInput(event) {
+        const { value } = event.target;
+        const cleanValue = value.replace(/[^0-9]/g, '');
+        inputValue = cleanValue;
+    }
+
+    function handleSubmit() {
+        const bet = {
+            type: selectedOption,
+            overUnder: selectedOverUnder,
+            value: inputValue
+        };
+    }
+</script>
+
+<div class="">
+    <h1>Select a Bet</h1>
+    <select bind:value={selectedOption}>
+        {#each options as option}
+            <option value={option}>{option}</option>
+        {/each}
+    </select>
+    <select bind:value={selectedOverUnder}>
+        {#each overUnder as option}
+            <option value={option}>{option}</option>
+        {/each}
+    </select>
+    <input type="number" bind:value={inputValue} on:input={cleanInput} />
+    <button on:click={handleSubmit}>Submit</button>
+</div>
---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+    import Roster from '$lib/components/Roster.svelte'
+    import Bet from '$lib/components/Bet.svelte'
+</script>
+
+<main class="flex justify-between h-screen items-center">
+    <div class="flex-1 border p-4 h-full">
+        <Roster/>
+    </div>
+    <div class="flex-1 border p-4 h-1/3">
+        <Bet/>
+    </div>
+    <div class="flex-1 border p-4 h-full">
+        <Roster/>
+    </div>
+</main>
