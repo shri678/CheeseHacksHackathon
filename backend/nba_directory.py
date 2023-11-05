a6ff6d5ea2b9cd50293d4c1dbf8d090d489ee746
shri678 <71198310+shri678@users.noreply.github.com>
2023-11-04 21:05:01 -0500

    Update nba_directory.py
    Added return_data_team() function

--- backend/nba_directory.py
+++ backend/nba_directory.py
@@ -42,3 +42,22 @@ def get_players_data(id):
     career_player.get_data_frames()[0]
     career_player.get_json()
     return career_player.get_dict()['resultSets'][0]['rowSet'][::-1][:2]
+
+def return_data_team(team_name):
+    team = teams.find_teams_by_full_name(regex_pattern=team_name)
+    team = team[0]
+    
+    play = commonteamroster.CommonTeamRoster(team_id=team['id'])
+    
+    play.get_data_frames()[0]
+    play.get_json()
+
+    team_info = play.get_dict()['resultSets'][0]['rowSet']
+
+    player_name_and_id = []
+    for player in team_info:
+        player_name_and_id.append({"name": player[3], "id":player[14]})
+
+    return player_name_and_id
+
+
