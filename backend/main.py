437e7a5292377b1db250b322829728246e8ba806
shri678 <71198310+shri678@users.noreply.github.com>
2023-11-04 21:03:56 -0500

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from algorithm import calculate_win_probability
 from nba_directory import get_players_data
+from nba_directory import return_data_team
+
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
@@ -14,24 +16,7 @@ app.add_middleware(
 # Returns the player names and ids for a given team
 @app.get("/teams/{team_name}")
 async def get_team(team_name: str):
-    team = teams.find_teams_by_full_name(regex_pattern=team_name)
-    team = team[0]
-
-    play = commonteamroster.CommonTeamRoster(team_id=team['id'])
-
-    play.get_data_frames()[0]
-
-    # json
-    play.get_json()
-
-    # dictionary
-    team_info = play.get_dict()['resultSets'][0]['rowSet']
-
-    player_name_and_id = []
-    for player in team_info:
-        player_name_and_id.append({"name": player[3], "id":player[14]})
-
-    return player_name_and_id
+    return return_data_team(team_name)
 
 #Returns the estimated win probability for a given bet
 @app.post("/bets/")
